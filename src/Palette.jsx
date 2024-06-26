f0bdee379f9f93e0e320e863be8b89a566b9abde
Shubham Singh <shubh0433@gmail.com>
2024-06-27 00:12:53 +0530

    create react-app color-fusion and added palette

---
+++ src/Palette.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Palette = () => {
+  return (
+    <div className="Palette">
+      {/* Navbar goes here*/}
+      <div className="Palette-colors">{/* bunch of color boxes */}</div>
+      {/* footer eventually */}
+    </div>
+  );
+};
+
+export default Palette;
